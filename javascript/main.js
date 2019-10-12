c291d5d5625d3db0e4ae6611dce3c6e256ed8d38
romain sanson <romainsanson.gm@gmail.com>
2019-10-12 16:55:03 +0200

    add owl

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,17 @@
-console.log('Hello World')
+// OWL
+
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+      items:1,
+      margin:0,
+      autoplay:true,
+      loop:true,
+      smartSpeed: 800,
+      // autoplayHoverPause: true,
+      autoHeight:true
+  });
+});
+
+// SLICK
+
+
